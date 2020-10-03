d5eeccca73dfe2276dc8eacdf3ecfa37aee5001d
kkm1999 <65722722+kkm1999@users.noreply.github.com>
2020-10-03 14:24:28 +0530

    Update Lexicographical order.cpp

--- Lexicographical order.cpp
+++ Lexicographical order.cpp
@@ -13,17 +13,17 @@ int main() {
    }
 
    // storing strings in the lexicographical order
-   for (int i = 0; i < 5; ++i) {
+   
       for (int j = i + 1; j < 5; ++j) {
 
          // swapping strings if they are not in the lexicographical order
          if (strcmp(str[i], str[j]) > 0) {
             strcpy(temp, str[i]);
-            strcpy(str[i], str[j]);
+            
             strcpy(str[j], temp);
          }
       }
-   }
+   
 
    printf("\nIn the lexicographical order: \n");
    for (int i = 0; i < 5; ++i) {
